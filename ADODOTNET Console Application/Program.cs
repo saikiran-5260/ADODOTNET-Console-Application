9d999a45915ba911784e12757990af70616ce46e
saikiran-5260 <shanmukha.pithani@accionlabs.com>
2023-03-20 11:22:27 +0530

    Commit 1

---
+++ ADODOTNET Console Application/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+using Microsoft.Data.SqlClient;
+
+SqlConnection con = null;
+con = new SqlConnection("server=(LocalDb)\\AL5260;database=MyLearnings;integrated security=SSPI;TrustedServerConnection=True;");
+SqlCommand sc = new SqlCommand("insert into Student(id,name,email,date_of_joining)values('101','Shanmukha Sai Kiran Pithani','saikiran@gmail.com','10/17/2022')", con);
+con.Open();
+sc.ExecuteNonQuery();
+con.Close();
+Console.WriteLine("Insert Query Executed successfully");
